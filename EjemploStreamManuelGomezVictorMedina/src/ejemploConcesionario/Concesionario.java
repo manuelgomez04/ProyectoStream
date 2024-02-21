f54611da18e7927d08894955eeee8f1fbdc0b890
Victor <victorml2204@gmail.com>
2024-02-21 20:18:08 +0100

    Metodos Víctor

--- EjemploStreamManuelGomezVictorMedina/src/ejemploConcesionario/Concesionario.java
+++ EjemploStreamManuelGomezVictorMedina/src/ejemploConcesionario/Concesionario.java
@@ -4,24 +4,26 @@ import java.util.OptionalDouble;
 
 public class Concesionario {
 
-	private CrudCoche cc;
-
-	// Calcular precio más descuentos o cosas raras (extras) --Víctor
+	private CrudCoche crudCoche;
 
 	// Contar vehículos vendidos (Count) --Víctor
+	public long contarCocheVendidos() {
+
+		return crudCoche.buscarVendidos().stream().count();
+	}
 
 	// Calcular ganancias si se venden todos (Map) --Manuel
 
 	public double calcularGanancias() {
 
-		return cc.getListaCoche().stream().mapToDouble(t -> t.getPrecio()).sum();
+		return crudCoche.getListaCoche().stream().mapToDouble(t -> t.getPrecio()).sum();
 	}
 
 	// Calcular media de precio de coches (Map x2) -- Manuel
 
 	public OptionalDouble calcularMediaPrecioCochesPorMarca(String modelo) {
 
-		return cc.getListaCoche().stream().mapToDouble(t -> t.getPrecio()).average();
+		return crudCoche.getListaCoche().stream().mapToDouble(t -> t.getPrecio()).average();
 	}
 
 	// Calcular media de precio de los coches de una marca (FlatMap) -- Manuel
--- EjemploStreamManuelGomezVictorMedina/src/ejemploConcesionario/CrudCoche.java
+++ EjemploStreamManuelGomezVictorMedina/src/ejemploConcesionario/CrudCoche.java
@@ -25,44 +25,54 @@ public class CrudCoche {
 		return "CrudCoche [listaCoche=" + listaCoche + "]";
 	}
 
-	// Buscar numBastidor, modelo (limit y skip) -- Manuel
-
-	// Método para buscar un coche por su número de bastidor
-
-	public Coche findByNumBastidor(String numBastidor) {
-
-		return listaCoche.stream().filter(t -> t.getNumBastidor().equalsIgnoreCase(numBastidor)).findFirst().get();
+	// Añadir coche -- Víctor
+	public void addCoche(Coche coche) {
+		listaCoche.add(coche);
 	}
-
-	// Método para buscar N coches por su modelo, siguiendo el orden de inserción
-
-	public List<Coche> findNModelos(String modelo, int limit) {
-
-		return listaCoche.stream().filter(t -> t.getModelo().equalsIgnoreCase(modelo)).limit(limit).toList();
-	}
-
-	public List<Coche> findModelos(String modelo) {
-		return listaCoche.stream().filter(t -> t.getModelo().equalsIgnoreCase(modelo)).toList();
-	}
-
-	// Método para buscar coches del mismo modelo, pero saltando N coches siguiendo
-	// el orden de insercion
-	public List<Coche> findListaModelosSkipN(String modelo, int skip) {
-
-		return listaCoche.stream().filter(t -> t.getModelo().equalsIgnoreCase(modelo)).skip(skip).toList();
-	}
-
-	// marca ( filter, find Any / First) -- Víctor
-
-	// Modificar precio u otras cosas -- Manuel
+	// Modificar precio -- Manuel
 
 	public void modificarPrecio(String numBastidor, double nuevoPrecio) {
 		findByNumBastidor(numBastidor).setPrecio(nuevoPrecio);
 	}
 
 	// Borrar coche --Víctor
+	public void borrarCoche(Coche coche) {
 
-	// Añadir coche -- Víctor
+		listaCoche.remove(coche);
+	}
+
+	// Mostrar coches (For each)-- Víctor
+	public void mostrarCoches() {
+		listaCoche.stream().forEach(System.out::println);
+	}
+
+	// Buscar numBastidor, modelo -- Manuel
+
+	// Método para buscar un coche por su número de bastidor
+
+	public Coche findByNumBastidor(String numBastidor) {
+
+		return listaCoche.stream().filter(coche -> coche.getNumBastidor().equalsIgnoreCase(numBastidor)).findFirst()
+				.get();
+	}
+
+	// Método para buscar N coches por su modelo, siguiendo el orden de inserción
+
+	public List<Coche> findNModelos(String modelo, int limit) {
+
+		return listaCoche.stream().filter(coche -> coche.getModelo().equalsIgnoreCase(modelo)).limit(limit).toList();
+	}
+
+	public List<Coche> findModelos(String modelo) {
+		return listaCoche.stream().filter(coche -> coche.getModelo().equalsIgnoreCase(modelo)).toList();
+	}
+
+	// Método para buscar coches del mismo modelo, pero saltando N coches siguiendo
+	// el orden de insercion
+	public List<Coche> findListaModelosSkipN(String modelo, int skip) {
+
+		return listaCoche.stream().filter(coche -> coche.getModelo().equalsIgnoreCase(modelo)).skip(skip).toList();
+	}
 
 	// Método para ordenar la lista de coches dada por el precio.
 
@@ -82,6 +92,14 @@ public class CrudCoche {
 
 	}
 
-	// Mostrar coches (For each)-- Víctor
+	// marca ( filter, find Any / First) -- Víctor
+	public List<Coche> buscarPorMarca(String marca) {
 
+		return listaCoche.stream().filter(coche -> coche.getMarca().equalsIgnoreCase(marca)).toList();
+	}
+
+	public List<Coche> buscarVendidos() {
+
+		return listaCoche.stream().filter(coche -> coche.isVendido()).toList();
+	}
 }
