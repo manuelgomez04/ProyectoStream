93f7600b5d9ae4d4230d6b4741596824da10627a
Victor <medina.levic24@triana.salesianos.edu>
2024-02-23 10:02:14 +0100

    Principal v3

--- EjemploStreamManuelGomezVictorMedina/src/ejemploConcesionario/Principal.java
+++ EjemploStreamManuelGomezVictorMedina/src/ejemploConcesionario/Principal.java
@@ -16,7 +16,7 @@ public class Principal {
 		String marca, modelo, numBastidor;
 		double caballos, precio;
 		boolean extras, vendido = false;
-		int opcion, LeerExtras, LeerVendido;
+		int opcion, opcion2, LeerExtras, LeerVendido;
 		Coche coche = new Coche("Seat", "Leon", "6425T", 450, 400000, true, false);
 		Coche coche1 = new Coche("Audi", "A4", "43643Y", 390, 250000, false, true);
 		Coche coche2 = new Coche("Peugeot", "P5", "7623U", 600, 590000, true, true);
@@ -35,6 +35,7 @@ public class Principal {
 					4 ---> Contar los coches vendidos
 					5 ---> Calcular el precio medio de los coches de una marca
 					6 ---> Ordenar coches por requisitos
+					7 ---> Borrar un coche
 					""");
 			opcion = Leer.datoInt();
 			switch (opcion) {
@@ -65,6 +66,26 @@ public class Principal {
 				System.out.println();
 				break;
 			case 2:
+				do {
+					System.out.println("""
+							1-Ordenar por precio
+							2-Ordenar por orden alfabetico
+							""");
+					opcion2 = Leer.datoInt();
+					switch (opcion2) {
+					case 1:
+						for (Coche coche : listaCoches) {
+
+						}
+						concesionario.ordenarPorPrecioMayorAMenor();
+						break;
+					case 2:
+						concesionario.ordenarPorOrdenAlfabeticoMarcaYModelo();
+						break;
+
+					}
+
+				} while (opcion2 != 0);
 
 				break;
 			case 3:
@@ -79,11 +100,25 @@ public class Principal {
 			case 5:
 				System.out.println("Intoduzca la marca para calcular el precio medio");
 				marca = Leer.dato();
-				System.out.println("El precio medio de los coches de la marca es: %.2f",
+				System.out.printf("El precio medio de los coches de la marca es: %.2f",
 						concesionario.calcularMediaPrecioCoches(marca));
-
+				System.out.println();
 				break;
 			case 6:
+
+				break;
+			case 7:
+				System.out.println("Introduzca el número de bastidos del coche que quiere borrar");
+				numBastidor = Leer.dato();
+				crudCoche.borrarCoche(numBastidor);
+				break;
+
+			default:
+				System.out.println("Introduzca una opción correcta");
+				System.out.println();
+				break;
+			case 0:
+				System.out.println("Saliendo...");
 				break;
 			}
 
